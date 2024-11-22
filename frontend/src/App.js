11defd74c9e555f3ac2076441fd5f5c0ad0dfe8e
Bashar-zakarneh <bashar.a.zakarneh@gmail.com>
2024-11-22 10:13:05 +0300

    Preparing the Project Structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import AdminPage from "./components/AdminPage";
+import HomePage from "./components/HomePage";
+import LoginForm from "./components/LoginForm";
+import RegistrationForm from "./components/RegistrationForm";
+import SummaryPage from "./components/SummaryPage";
+
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-cover h-screen  bg-gradient-to-r from-cyan-500 to-blue-500">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <RegistrationForm />
+      <LoginForm />
+      <HomePage />
+      <SummaryPage />
+      <AdminPage />
     </div>
   );
 }
